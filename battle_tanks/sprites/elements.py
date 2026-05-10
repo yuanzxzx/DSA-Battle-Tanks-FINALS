8cd20e47b528a97e98da1afd290e3c8abcdeb9be
zmonk966 <zaymondg@gmail.com>
2026-05-10 15:59:41 +0800

    Implement Bullet class for shooting mechanics
    Added Bullet class to handle bullet creation and movement.

--- battle_tanks/sprites/elements.py
+++ battle_tanks/sprites/elements.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import math
 from battle_tanks import ROUTE
 
 
@@ -38,5 +39,22 @@ class Block(SpriteBasic):
     def __init__(self,x_pos,y_pos,width,height):
         super().__init__(x_pos,y_pos,width,height)
 
+class Bullet(pg.sprite.Sprite):
+    def __init__(self, x, y, angle_cannon, max_distance=300):
+        super().__init__()
+        self.image = pg.Surface((6, 6), pg.SRCALPHA)
+        pg.draw.circle(self.image, (255, 255, 0), (3, 3), 3) # Yellow bullet
+        self.rect = self.image.get_rect(center=(x, y))
+        self.distance_traveled = 0
+        self.max_distance = max_distance
+        
+        radian_angle = math.radians(angle_cannon)
+        self.vx = -math.sin(radian_angle) * 15
+        self.vy = -math.cos(radian_angle) * 15
 
-
+    def update(self):
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+        self.distance_traveled += math.sqrt(self.vx**2 + self.vy**2)
+        if self.distance_traveled >= self.max_distance:
+            self.kill()
